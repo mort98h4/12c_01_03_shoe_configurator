c18f55d98f5f78af72d84d4e6e2317b8cb9cfe08
Morten Gross <mg@vuffeli.dk>
2021-03-02 17:09:33 +0100

    The svg is now fetched via JS

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
     console.log("init");
+    const response = await fetch("jordan-01.svg");
+    const svgData = await response.text();
+
+    document.querySelector("#shoeSVG").innerHTML = svgData;
 }
