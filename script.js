a9de9b7551ff2e5c75022637cc84a493c1fde955
Morten Gross <mg@vuffeli.dk>
2021-03-02 17:32:56 +0100

    highlighting the selected area is now working

--- script.js
+++ script.js
@@ -8,4 +8,26 @@ async function init() {
     const svgData = await response.text();
 
     document.querySelector("#shoeSVG").innerHTML = svgData;
+    makeInteractive();
+}
+
+function makeInteractive() {
+    console.log("makeInteractive");
+    document.querySelectorAll(".interactive_group").forEach(group => {
+        group.addEventListener("mouseover", highlightPath);
+        group.addEventListener("click", clickOnGroup);
+    })
+}
+
+function highlightPath() {
+    console.log("highlightPath");
+    this.style.stroke = "#000000";
+    this.style.strokeWidth = "2";
+    this.addEventListener("mouseout", () => {
+        this.style.stroke = "none";
+    })
+}
+
+function clickOnGroup() {
+    console.log("clickOnGroup");
 }
