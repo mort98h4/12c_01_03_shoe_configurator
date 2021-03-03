b63bd427748b636cd70812de444fe4996635b907
Morten Gross <mg@vuffeli.dk>
2021-03-03 19:39:37 +0100

    ... and now the third and final solution is done

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", init);
 
 let elementToColor;
 let colorValue;
+let id;
 
 async function init() {
     console.log("init");
@@ -20,11 +21,7 @@ function makeInteractive() {
         group.addEventListener("mouseover", highlightPath);
         group.addEventListener("click", clickOnGroup);
     })
-    // document.querySelectorAll(".color_btn").forEach(button => {
-    //     button.addEventListener("click", clickOnColor);
-    // })
     document.querySelector("input").addEventListener("input", selectColor);
-    document.querySelector("button").addEventListener("click", clickSubmitColor);
 }
 
 function highlightPath() {
@@ -38,41 +35,36 @@ function highlightPath() {
 
 function clickOnGroup() {
     console.log("clickOnGroup");
+    console.log(this.dataset.id);
+    id = this.dataset.id;
     elementToColor = this;
     this.style.fill = "grey";
 }
-
-// function clickOnColor() {
-//     console.log("clickOnColor");
-//     if (elementToColor != undefined) {
-//         elementToColor.style.fill = this.getAttribute("fill");
-//     }
-// }
 
 function selectColor() {
     console.log("colorSelect");
     if (elementToColor != undefined) {
         colorValue = document.querySelector("input").value;
         elementToColor.style.fill = colorValue;
-        
-        // function clickSubmitColor() {
-        //     document.querySelector("button").removeEventListener("click", clickSubmitColor);
-        //     submitColor(colorValue);
-        // }
+        displayColor();
     }
 }
 
-function clickSubmitColor() {
+function displayColor() {
     console.log(colorValue);
-    const colorList = document.querySelector(".color_list");
-    
-    
+    const rgba = hexToRGBA(colorValue)
 
-    const newColor = document.createElement("div");
-    newColor.innerHTML = `<div class="colorBox" style="background-color: ${colorValue}"></div><p class="hexValue">${colorValue.toUpperCase()}</p>`;
-    colorList.appendChild(newColor);
-    console.log(colorList.childElementCount);
-    if (colorList.childElementCount >= 10) {
-        colorList.removeChild(colorList.childNodes[0]);
-    }
+    document.querySelector(`.colorBox${id}`).style.backgroundColor = rgba;
+    document.querySelector(`.colorBox${id}`).style.border = `1px solid ${colorValue}`;
+    document.querySelector(`.hex${id}`).textContent = colorValue;
+    document.querySelector(`.hex${id}`).style.color = colorValue;
+}
+
+function hexToRGBA(hex) {
+    hex = hex.substring(1);
+    const r = Number.parseInt(hex.substring(0, 2), 16);
+    const g = Number.parseInt(hex.substring(2, 4), 16);
+    const b = Number.parseInt(hex.substring(4, 6), 16);
+    const a = 0.5
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
